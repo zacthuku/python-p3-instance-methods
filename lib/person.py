8652a08af8b86b9e6c316cb8b98f41f0f66b235f
Zacharia Ndinguri <zacthuku@gmail.com>
2025-05-10 08:12:40 +0300

    instance methods

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,12 @@
 
 class Person:
     # Class body goes here
-
+    def talk(self):
+        print('Hello World!')
+    def walk(self):
+        print('The person is walking.')
     #Instance method definition
-    pass
+guido=Person()
+guido.talk()
+
+pass
